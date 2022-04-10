621b0432532be464ff315faf5af7ccc70fe00b8b
Jose Lemuz <lemuz.jose@gmail.com>
2022-04-09 17:39:08 -0700

    cleaned up fles

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -39,38 +39,6 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
-router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findByPk(req.params.id, {
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at'
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (dbPostData) {
-        const post = dbPostData.get({ plain: true });
-        
-        res.render('edit-post', {
-          post,
-          loggedIn: true
-        });
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
-});
-
 router.post('/', withAuth, async (req,res)=>{ 
   console.log('======================Posting Image');
   try{
